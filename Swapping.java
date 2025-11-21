9c981a7d56c5de8eb30754e6581f9ec0d232c6f2
SupriyaKoduru4 <kodurusupriya4@gmail.com>
2025-11-22 00:06:56 +0530

    added swapping

---
+++ Swapping.java
@@ -0,0 +1,21 @@
+// swapping 2 numbers 
+public class Swapping {
+
+    public static void main(String[] args) {
+        int[] marks = {20, 39, 37, 46, 28};
+
+        // assign a number to one index
+        int i = 1;
+        int j = 4;
+
+        //swapping the numbers
+        int temp = marks[i];
+        marks[i] = marks[j];
+        marks[j] = temp;
+
+        for (int num : marks) {
+            System.out.println(num);
+
+        }
+    }
+}
